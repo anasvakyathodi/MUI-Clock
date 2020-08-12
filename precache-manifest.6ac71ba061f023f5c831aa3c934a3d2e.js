ba07f1c905619b3dd6eedfdb6f30898672bb7090
anasvakyathodi <anasvakyathodi@gmail.com>
2020-08-12 12:20:29 +0530

    Updates

---
+++ precache-manifest.6ac71ba061f023f5c831aa3c934a3d2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe67a87e7ded23c045375a7f1e693bab",
+    "url": "/MUI-Clock/index.html"
+  },
+  {
+    "revision": "4314585a6e9829ccf8da",
+    "url": "/MUI-Clock/static/js/2.70bf5213.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/MUI-Clock/static/js/2.70bf5213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe835a234462fe3053f2",
+    "url": "/MUI-Clock/static/js/main.9e3ef62d.chunk.js"
+  },
+  {
+    "revision": "f78b2a4762c6f28580d2",
+    "url": "/MUI-Clock/static/js/runtime-main.3b923b9f.js"
+  }
+]);
